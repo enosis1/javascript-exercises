37b7e6629f400719e78486869f10c444a0632e9e
Mike Johnson <72291166+enosis1@users.noreply.github.com>
2023-09-06 17:52:49 -0400

    Pass unit tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,13 +1,6 @@
 const removeFromArray = function (array, ...args) {
-  for (arg of args) {
-    for (let i = 0; i < array.length; i++) {
-      if (array[i] === arg) {
-        array.splice(i, 1);
-      }
-    }
-  }
-  return array;
-};
+    return array.filter(element => !args.includes(element))
+}
 
 removeFromArray([1, 2, 3, 4], 3, 4, 1);
 // Do not edit below this line
