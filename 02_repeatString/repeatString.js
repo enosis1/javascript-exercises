748e80f69a991ec6d8e5ed119340926f8e74a3cc
Mike Johnson <72291166+enosis1@users.noreply.github.com>
2023-09-06 16:44:00 -0400

    Pass unit tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+  let addedString = "";
+  if (num < 0) {
+    return "ERROR";
+  } else {
+    for (let i = 0; i < num; i++) {
+      addedString += string;
+    }
+  }
+  return addedString;
 };
 
 // Do not edit below this line
