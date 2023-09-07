99f5e26f3131ff87822a10122200111c7b359c96
Mike Johnson <72291166+enosis1@users.noreply.github.com>
2023-09-06 21:36:44 -0400

    Pass unit tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,23 @@
-const sumAll = function() {
+const sumAll = function (numOne, numTwo) {
+  let sum = 0;
 
+  if (!Number.isInteger(numOne) || !Number.isInteger(numTwo)) {
+    return "ERROR";
+  } else if (numOne < 0 || numTwo < 0) {
+    return "ERROR";
+  } else if (numOne > numTwo) {
+    for (i = 0; i <= numOne; i++) {
+      sum += i;
+    }
+    return sum;
+  } else {
+    for (i = numTwo; i >= numOne; i--) {
+      sum += i;
+    }
+  }
+  return sum;
 };
 
+sumAll(90, 40);
 // Do not edit below this line
 module.exports = sumAll;
