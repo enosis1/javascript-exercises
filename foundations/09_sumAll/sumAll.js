aa1f94ce5cc3d47e98add4935dbee25856efc519
Mike Johnson <72291166+enosis1@users.noreply.github.com>
2026-02-21 14:18:51 -0500

    Solve sumAll.js

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+const sumAll = function (a, b) {
+  if (a < 0 || b < 0) {
+    return "ERROR";
+  }
 
+  if (typeof a !== "number" || typeof b !== "number") {
+    return "ERROR";
+  }
+
+  if (!Number.isInteger(a) || !Number.isInteger(b)) {
+    return "ERROR";
+  }
+
+  let sum = 0;
+  if (a > b) {
+    let oldA = a;
+    a = b;
+    b = oldA;
+  }
+  for (let i = a; i <= b; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
