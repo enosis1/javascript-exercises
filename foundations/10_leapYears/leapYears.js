f3c36adc64c9bfa00d4be18b94a2011a20dc8d51
Mike Johnson <72291166+enosis1@users.noreply.github.com>
2026-02-22 13:12:58 -0500

    Solve leapYears.js

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,6 +1,19 @@
-const leapYears = function() {
-
+const leapYears = function (year) {
+  if (year % 100 === 0) {
+    if (year % 400 === 0) {
+      return true;
+    } else {
+      return false;
+    }
+  } else if (year % 4 === 0) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
+const leapYearsBetter = function (year) {
+  return (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0;
+};
 // Do not edit below this line
 module.exports = leapYears;
