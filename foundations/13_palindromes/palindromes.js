934a65fdab08c9339bf4949f33a2e67975ef8f23
Mike Johnson <72291166+enosis1@users.noreply.github.com>
2026-03-07 20:23:44 -0500

    Solve palindromes.js

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,6 +1,19 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+  const formattedWord = word
+    .replace(/\s/g, "")
+    .replace(/[^a-zA-Z0-9]/g, "")
+    .toLowerCase();
+  const flippedWord = word
+    .replace(/\s/g, "")
+    .replace(/[^a-zA-Z0-9]/g, "")
+    .toLowerCase()
+    .split("")
+    .reverse()
+    .join("");
+  return formattedWord === flippedWord ? true : false;
 };
+
+palindromes("A car, a man, a maraca.");
 
 // Do not edit below this line
 module.exports = palindromes;
