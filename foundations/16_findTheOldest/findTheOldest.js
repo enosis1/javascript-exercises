09b076a1d8db872004dc30d17b4a3b0fe21f3603
Mike Johnson <72291166+enosis1@users.noreply.github.com>
2026-03-07 22:00:58 -0500

    Solve findTheOldest.js

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (array) {
+  let peopleAges = array.map((item) => ({
+    name: item.name,
+    age: item.yearOfDeath
+      ? item.yearOfDeath - item.yearOfBirth
+      : new Date().getFullYear() - item.yearOfBirth,
+  }));
+  let sortedPeople = peopleAges.sort((a, b) => b.age - a.age);
+  return sortedPeople[0];
 };
 
 // Do not edit below this line
