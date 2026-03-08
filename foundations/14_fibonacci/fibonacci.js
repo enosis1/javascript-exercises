7762cc71a2030b56b65052cc73dde74c72a021d9
Mike Johnson <72291166+enosis1@users.noreply.github.com>
2026-03-07 21:04:30 -0500

    Kinda solve fibonacci.js

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,6 +1,24 @@
-const fibonacci = function() {
-
+const fibonacci = function (number) {
+  const testNumber = Number(number);
+  if (testNumber === 0) {
+    return 0;
+  } else if (testNumber === 1) {
+    return 1;
+  } else if (testNumber < 0) {
+    return "OOPS";
+  } else {
+    let prev = 0;
+    let current = 1;
+    for (let i = 2; i <= testNumber; i++) {
+      let next = prev + current;
+      prev = current;
+      current = next;
+    }
+    return current;
+  }
 };
+
+console.log(fibonacci(10));
 
 // Do not edit below this line
 module.exports = fibonacci;
